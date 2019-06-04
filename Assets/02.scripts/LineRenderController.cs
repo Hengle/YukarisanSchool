27f1b12687b35a73c14118225805f1f0dfec8d99
sadxws <sadxws@hotmail.com>
2019-06-04 23:48:38 +0900

    선 추가중
    선 추가중

---
+++ Assets/02.scripts/LineRenderController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRenderController : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lineRenderer_ = default;
+    [SerializeField] private List<Transform> listPos_ = default;
+    public bool isEditMode_ = false;
+    public float width_ = 0.2f;
+
+    private void Awake()
+    {
+        foreach (Transform tr in listPos_)
+        {
+            tr.gameObject.GetComponent<LineRendererPos>().lineRendererCtrl_ = this;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        SetLineRender();
+    }
+
+
+    public void SetLineRender()
+    {
+        lineRenderer_.startWidth = width_; 
+        lineRenderer_.endWidth = width_;
+
+
+        for (int index = 0; index < listPos_.Count; index++)
+        {
+            lineRenderer_.SetPosition(index, listPos_[index].position);
+        }
+    }
+
+}
